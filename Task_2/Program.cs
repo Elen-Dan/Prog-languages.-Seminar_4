34dd7a1158d0416dec6416ffd18c35104a8509e2
El_Dan <lilit-li@ya.ru>
2022-09-01 20:51:38 +0400

    Добавление задачи 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 2: 
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе, которые являются делителями этого числа. 
+(для получения цифр числа операцию приведения числа к строке не использовать)
+*/
+
+Console.Clear();
+
+// конвертация string в int
+int ReadInt (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Number = ReadInt("Введите число: ");
+int tempNum = Number;
+int Sum = 0; //сумма требуемых цифр в числе
+int Ostatok = 0; // остаток введенного Числа (number) от деления на 10
+
+while (tempNum != 0) {
+Ostatok = tempNum % 10;
+if(Ostatok == 0)
+   Console.WriteLine("Как еще бороться с делением на ноль в 24-й строке???");          
+else if(tempNum % Ostatok == 0) 
+   {
+   Sum = Sum + Ostatok;         
+   }
+tempNum = tempNum / 10;
+
+}         
+Console.WriteLine("Сумма цифр делителей числа: " + Sum);
+Console.WriteLine(2 % 2);
